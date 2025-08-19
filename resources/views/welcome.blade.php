6f9e167221a7af5b6853ca43231b00a866907c96
jasanzdev <sanz_cuesta94@hotmail.com>
2025-08-19 19:06:02 -0300

    Installed and configure laravelpwa package

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -14,6 +14,8 @@
     <link rel="preconnect" href="https://fonts.bunny.net">
     <link href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600" rel="stylesheet"/>
 
+    @laravelPWA
+
     <!-- Styles -->
     <style>
         /*! tailwindcss v4.0.14 | MIT License | https://tailwindcss.com */
